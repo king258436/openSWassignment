114cbe68ea1246e7d38641eb6fd397a6ebe2fb96
최원식 <74888408+king258436@users.noreply.github.com>
2023-04-07 09:32:07 +0900

    Create SelfStudy 2.py

---
+++ week5/chapter10_WindowPrograming/SelfStudy 2.py
@@ -0,0 +1,30 @@
+from tkinter import *
+import random
+btnList = [None] * 9
+fnameList = ["a.gif", "b.gif", "c.gif",
+            "d.gif", "e.gif", "f.gif",
+            "g.gif", "h.gif", "i.gif"]
+photoList = [None]*9
+i, k = 0, 0
+xPos, yPos = 0, 0
+num = 0
+
+window = Tk()
+window.geometry("210x210")
+
+for i in range(0, 9):
+    photoList[i] = PhotoImage(
+        file="/Users/wonsick/Documents/ImageFile/"+fnameList[i])
+    btnList[i] = Button(window, image=photoList[i])
+    
+random.shuffle(btnList)
+
+for i in range(0, 3):
+    for k in range(0, 3):
+        btnList[num].place(x=xPos, y=yPos)
+        num += 1
+        xPos += 70
+    xPos = 0
+    yPos += 70
+
+window.mainloop()
