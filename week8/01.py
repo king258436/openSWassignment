430d857868f89835bb8bbe5b9d3f3455071f9f45
최원식 <74888408+king258436@users.noreply.github.com>
2023-04-27 22:52:16 +0900

    Create 01.py

---
+++ week8/01.py
@@ -0,0 +1,20 @@
+from typing import MutableSequence
+def bubble_sort(a: MutableSequence) -> None:
+    n = len(a)
+    for i in range(n-1):
+        for j in range(n-1, i, -1):
+            if a[j-1] > a[j]:
+                a[j-1], a[j] = a[j], a[j-1]
+if __name__ == '__main__':
+    print('버블 정렬을 수행합니다.')
+    n = int(input("원소 수를 입력하세요. : "))
+    x = [0]*n
+
+    for i in range(n):
+        x[i] = int(input(f'x[{i}]:'))
+
+    bubble_sort(x)
+
+    print('오름차순으로 정렬했습니다. ')
+    for i in range(n):
+        print(f'x[{i}] = {x[i]}')
